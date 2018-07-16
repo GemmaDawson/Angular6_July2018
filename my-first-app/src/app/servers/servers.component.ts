464e08edb221b47e7ded8c8243849b685bd07667
GemmaDawson <gemma@icepack.co>
2018-07-16 13:57:00 +0200

    Section 2 Lecture 21 | Component Selector
    Changing the selector to being an attribute

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-servers",
+  // adding square brackets changes the selector to be an attribute
+  // this breaks the app as the code does not recognise <app-servers> anymore
+  // adding line 7 in app.component.html fixes the app
+  selector: "[app-servers]",
   template: `
   <app-server></app-server>
   <app-server></app-server>`,

7701ed56001a7041cb265c5a420e29756f7fe933
GemmaDawson <gemma@icepack.co>
2018-07-16 14:03:54 +0200

    Section 2 Lecture 21 | Component Selector
    Back to the original selector

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,11 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  // adding square brackets changes the selector to be an attribute
-  // this breaks the app as the code does not recognise <app-servers> anymore
-  // adding line 7 in app.component.html fixes the app
-  // selector: "[app-servers]",
-  selector: ".app-servers",
+  selector: "app-servers",
   template: `
   <app-server></app-server>
   <app-server></app-server>`,

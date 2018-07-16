b75d2b6c8724bb64d02e9779fe39a5bd3984f8b9
GemmaDawson <gemma@icepack.co>
2018-07-16 14:52:11 +0200

    Assignment 1 | Add warning component & CSS

--- assignment1/src/app/app.module.ts
+++ assignment1/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpModule } from "@angular/http";
 
 import { AppComponent } from "./app.component";
 import { SuccessComponent } from "./success/success.component";
+import { WarningComponent } from './warning/warning.component';
 
 @NgModule({
-  declarations: [AppComponent, SuccessComponent],
+  declarations: [AppComponent, SuccessComponent, WarningComponent],
   imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
--- assignment1/src/app/success/success.component.ts
+++ assignment1/src/app/success/success.component.ts
@@ -3,6 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-success",
   templateUrl: "./success.component.html"
-  //   styleUrls: ["./success.component.css"]
+  styles: ["p{color:green}"]
 })
 export class SuccessComponent {}

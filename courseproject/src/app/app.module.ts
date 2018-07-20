29dc778781b09f28a00f2eacaf0ebd07c0261d67
GemmaDawson <gemma@icepack.co>
2018-07-20 18:05:29 +0200

    Section 3 Lecture 43 | Creating the Components
    Create component folders and files as planned in Lecture 40
    Call on the components

--- courseproject/src/app/app.module.ts
+++ courseproject/src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ShoppinglistComponent } from "./shopping-list/shoppinglist/shoppinglist.component";
+import { ShoppinglisteditComponent } from "./shopping-list/shoppinglistedit/shoppinglistedit.component";
+import { RecipelistComponent } from "./recipe-book/recipelist/recipelist.component";
+import { RecipeitemComponent } from "./recipe-book/recipeitem/recipeitem.component";
+import { RecipedetailComponent } from "./recipe-book/recipedetail/recipedetail.component";
+import { HeaderComponent } from "./header/header/header.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShoppinglistComponent,
+    ShoppinglisteditComponent,
+    RecipelistComponent,
+    RecipeitemComponent,
+    RecipedetailComponent,
+    HeaderComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

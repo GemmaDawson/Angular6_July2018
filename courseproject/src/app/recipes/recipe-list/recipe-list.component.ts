01013d2f156d16c163d94a7c5bf48e80875008ab
GemmaDawson <gemma@icepack.co>
2018-07-23 14:11:41 +0200

    Section 3 Lecture 48 | Adding Content to the Recipes Components
    Setting up the recipe-list component to display the available recipes

--- courseproject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ courseproject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
   selector: "app-recipe-list",
@@ -6,7 +7,13 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      "Doughnut",
+      "Doughnut Description",
+      "https://s1.piq.land/2012/09/02/hShZ2cTlaKWQjmozoMoxvGA5_400x400.png"
+    )
+  ];
 
   constructor() {}
 

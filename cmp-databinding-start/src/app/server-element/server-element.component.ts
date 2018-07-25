ebce1d94a57912db735d3e480115f748871f80b2
GemmaDawson <gemma@icepack.co>
2018-07-25 17:13:31 +0200

    Section 5 Lecture 68 | More on View Encapsulation
    ViewEncapsulation.Native = uses shadow DOM technology & gives some result as using ViewEncapsulation.Emulated BUT only for certain browsers.
    TL;DR - use .Emulated or .None as needed

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit, Input, ViewEncapsulation } from "@angular/core";
   selector: "app-server-element",
   templateUrl: "./server-element.component.html",
   styleUrls: ["./server-element.component.css"],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Native
 })
 export class ServerElementComponent implements OnInit {
   @Input("srvElement") element: { type: string; name: string; content: string };

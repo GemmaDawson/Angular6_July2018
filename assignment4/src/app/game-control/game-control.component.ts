b23ad1be007c9762ae86895e32f57dff8d28401f
GemmaDawson <gemma@icepack.co>
2018-07-26 09:40:56 +0200

    Assignment 4 | Question 2
    Game Control contains START and STOP buttons.

--- assignment4/src/app/game-control/game-control.component.ts
+++ assignment4/src/app/game-control/game-control.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-game-control',
-  templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  selector: "app-game-control",
+  templateUrl: "./game-control.component.html",
+  styleUrls: ["./game-control.component.css"]
 })
-export class GameControlComponent implements OnInit {
+export class GameControlComponent {
+  displayText: string = "Click the START button to begin!";
 
-  constructor() { }
-
-  ngOnInit() {
+  onStartGame() {
+    return (this.displayText = "It has begun!");
   }
 
+  onStopGame() {
+    return (this.displayText = "Game Over! Click the START button to begin!");
+  }
 }

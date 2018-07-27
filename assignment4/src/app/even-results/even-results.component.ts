513c07d25f69015d5e40403da6f1843b20d242b7
GemmaDawson <gemma@icepack.co>
2018-07-27 09:05:00 +0200

    Assignment 4 | Question 6 & 7

--- assignment4/src/app/even-results/even-results.component.ts
+++ assignment4/src/app/even-results/even-results.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-even-results',
-  templateUrl: './even-results.component.html',
-  styleUrls: ['./even-results.component.css']
+  selector: "app-even-results",
+  templateUrl: "./even-results.component.html",
+  styleUrls: ["./even-results.component.css"]
 })
 export class EvenResultsComponent implements OnInit {
+  @Input() number: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- assignment4/src/app/odd-results/odd-results.component.ts
+++ assignment4/src/app/odd-results/odd-results.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-odd-results',
-  templateUrl: './odd-results.component.html',
-  styleUrls: ['./odd-results.component.css']
+  selector: "app-odd-results",
+  templateUrl: "./odd-results.component.html",
+  styleUrls: ["./odd-results.component.css"]
 })
 export class OddResultsComponent implements OnInit {
+  @Input() number: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }

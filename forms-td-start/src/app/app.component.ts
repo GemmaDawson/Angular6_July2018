96ad5f10d4db1f774be0eafcd6fa917574eccfe3
GemmaDawson <gemma@icepack.co>
2018-09-06 10:02:54 +0200

    Section 15 Lecture 177 | Passing the form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef } from "@angular/core";
 
 @Component({
   selector: "app-root",
@@ -10,7 +10,7 @@ export class AppComponent {
     const suggestedName = "Superuser";
   }
 
-  onSubmit() {
-    console.log("Submitted!");
+  onSubmit(form: ElementRef) {
+    console.log(form);
   }
 }

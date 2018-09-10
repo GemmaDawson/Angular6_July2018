221b65c8ee6e423e5213b818fdf33e26c20588ea
GemmaDawson <gemma@icepack.co>
2018-09-10 08:27:00 +0200

    Section 15 Lecture 180 | TD: Adding Validation to Check User Input
    Clean up from Lecture 179

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component, ViewChild } from "@angular/core";
+import {
+  Component
+  // , ViewChild
+} from "@angular/core";
 import { NgForm } from "@angular/forms";
 
 @Component({
@@ -7,18 +10,18 @@ import { NgForm } from "@angular/forms";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  @ViewChild("f")
+  // @ViewChild("f")
   signupForm: NgForm;
 
   suggestUserName() {
     const suggestedName = "Superuser";
   }
 
-  onSubmit() {
-    console.log(this.signupForm);
-  }
-
-  // onSubmit(form: NgForm) {
-  //   console.log(form);
+  // onSubmit() {
+  //   console.log(this.signupForm);
   // }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
 }

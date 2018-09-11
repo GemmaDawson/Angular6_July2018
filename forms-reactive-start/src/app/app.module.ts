2e2eced80c01e7db822b00d21bfaa94851021536
GemmaDawson <gemma@icepack.co>
2018-09-11 07:59:32 +0200

    Section 15 Lecture 192 | Reactive: Setup
    Create a new property to hold the signup form

--- forms-reactive-start/src/app/app.module.ts
+++ forms-reactive-start/src/app/app.module.ts
@@ -1,20 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { ReactiveFormsModule } from "@angular/forms";
+import { AppComponent } from "./app.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ReactiveFormsModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}

7e4fc12f2f7c279b11df1f8c6922d255a92eba89
GemmaDawson <gemma@icepack.co>
2018-09-11 09:28:29 +0200

    Section 15 Lecture 204 | Reactive: Setting and Patching Values
    Setting values

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -30,6 +30,14 @@ export class AppComponent implements OnInit {
     });
     // this.signupForm.valueChanges.subscribe(value => console.log(value));
     this.signupForm.statusChanges.subscribe(status => console.log(status));
+    this.signupForm.setValue({
+      userData: {
+        username: "Max",
+        email: "max@email"
+      },
+      gender: "male",
+      hobbies: []
+    });
   }
 
   onSubmit() {

335f2578783c6650f02ab7125113802eba992540
GemmaDawson <gemma@icepack.co>
2018-09-11 07:03:33 +0200

    Section 15 Lecture 189 | TD: Using Form Data
    How to access and display the data from the form for the user

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
   defaultQuestion = "pet";
   answer = "";
   genders = ["female", "male"];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion: "",
+    answer: "",
+    gender: ""
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = "Superuser";
@@ -32,7 +40,12 @@ export class AppComponent {
   }
 
   onSubmit() {
-    console.log(this.signupForm);
+    this.submitted = !this.submitted;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 
   // onSubmit(form: NgForm) {
